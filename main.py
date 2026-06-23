9785ba3e3b4c7f3b9535720358c36021e9e0e389
poojakshu2424-prog <poojakshu2424@gmail.com>
2026-06-23 21:06:22 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+number = int(input("Enter a number: "))
+
+result = check_even_odd(number)
+print("The number", number, "is", result)
